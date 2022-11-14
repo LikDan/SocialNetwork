5beb5d3e3ce148e24f866fbea3a207ae6691cc25
LikDan <likdan.company@gmail.com>
2022-11-14 18:21:00 +0300

    Add iphone mime types

--- app/Http/Requests/Api/v1/AttachmentRequest.php
+++ app/Http/Requests/Api/v1/AttachmentRequest.php
@@ -9,7 +9,8 @@ class AttachmentRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "file" => 'required|file|mimetypes:image/gif,audio/mpeg,audio/mpeg,video/mpeg,image/png,image/jpeg',
+            "file" => 'required|file|mimetypes:image/gif,audio/mpeg,audio/mpeg,video/mpeg,image/png,image/jpeg,application/x-mpegURL,video/MP2T',
+            //https://stackoverflow.com/questions/22378742/laravel-video-file-validation
         ];
     }
 }
--- app/Http/Requests/Api/v1/AvatarRequest.php
+++ app/Http/Requests/Api/v1/AvatarRequest.php
@@ -9,7 +9,8 @@ class AvatarRequest extends FormRequest
     public function rules(): array
     {
         return [
-            "avatar" => "required|file|mimetypes:image/jpeg,image/png,image/gif"
+            "avatar" => "required|file|mimetypes:image/jpeg,image/png,image/gif,application/x-mpegURL"
+            //https://stackoverflow.com/questions/22378742/laravel-video-file-validation
         ];
     }
 }
